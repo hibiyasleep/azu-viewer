d3215a040fde87ea09c471172fe77ec1210486ac
Kuriyama Hibiya <kuriyama@hibiya.moe>
2016-02-08 01:15:56 +0900

    js

---
+++ public/js/sdvx-list.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.addEventListener('load', function() {
+  var l = new List('body', {
+    valueNames: [
+      'list-title', 'list-artist', 'list-level', 'list-played',
+      'list-clear', 'list-rank', 'list-score', 'list-clearrate'
+    ]
+  })
+})
